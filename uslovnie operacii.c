099e29396d0f306cc8f99e7e84f4589578a3a483
DMITRIY0510 <69233033+DMITRIY0510@users.noreply.github.com>
2020-08-10 12:34:53 +0500

    Add files via upload

--- uslovnie operacii.c
+++ uslovnie operacii.c
@@ -2,11 +2,17 @@
 #include<stdbool.h>
 
 int main() {
-	int a, b, c, d;
-	scanf_s("%d%d%d%d", &a,&b,&c,&d);
-	printf("%d", (a / b) > (c / d));
+	int a = 1, b = 0, c = 0;
+	scanf_s("%d%d%d",&a,&b,&c);
+	printf("%d", a || b && !c);
+	printf("%d", !a && !b);
+	printf("%d", !(a && c) || b);
+	printf("%d", a && !b || c);
+
+     
+	
 		
 	
-	
+
 	return 0;
 } 
